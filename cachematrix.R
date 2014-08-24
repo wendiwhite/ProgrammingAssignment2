42aed59f29769381557163895b91b2f557eb9aaa
wendiwhite <wendi.white@gmail.com>
2014-08-24 15:30:23 -0500

    Programming Assignment 2 edits
    Wendi White

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions caches the inverse of a matrix for reuse, offsetting the need to repeat this costly computation.
+    
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = Matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
+## Step 1: Set the value of the matrix
+        InverseMatrix <- NULL
+        set <- function(y) {
+                x <<- y
+                InverseMatrix <<- NULL
+        }
 
+## Step 2: Get the value of the matrix
+        get <- function() x
+
+## Step 3: Set the inverse value of the matrix
+        setInverseMatrix <- function(c) InverseMatrix <<- c
+
+## Step 4: Get the inverse value of the matrix
+        getInverseMatrix <- function() InverseMatrix
+
+## Step 5: Coerce all values into a list
+        list(set = set, get = get, setInverseMatrix = setInverseMatrix, getInverseMatrix = getInverseMatrix)
 }
 
-
-## Write a short comment describing this function
+##  cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## Step 1: Get the cached inverse value of the matrix
+        InverseMatrix <- x$getInverseMatrix()
+        
+## Step 2: Test if value is null, and return the value otherwise
+        if(!is.null(InverseMatrix)) {
+                message("getting cached value of inverse matrix")
+                return(InverseMatrix)
+        }
+
+## Step 3: If value was null, calculate the value
+        message("computing value of inverse matrix")
+        Matrix <- x$get()
+        InverseMatrix <- solve(Matrix, ...)
+
+## Step 4: Cache the calculated value
+        x$setInverseMatrix(InverseMatrix)
+        InverseMatrix
+        
 }
